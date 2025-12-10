33f3e97c1cf24cd151b16795c264ff1204698fa0
Mateus <mateus999391343@gmail.com>
2025-12-09 21:52:34 -0300

    fix: Corrigindo bug do Demo

--- ex_abstract_factory/Demo.java
+++ ex_abstract_factory/Demo.java
@@ -2,25 +2,22 @@ import java.util.Scanner;
 
 public class Demo {
 
-    /**
-     * Application picks the factory type and creates it in run time (usually at
-     * initialization stage), depending on the configuration or environment
-     * variables.
-     */
     private static Application configureApplication() {
         Application app;
         GUIFactory factory;
+
         Scanner scan = new Scanner(System.in);
-        System.out.println("Qual o estilo de moveis:");
+        System.out.println("Qual o estilo de móveis?");
         String movel = scan.nextLine();
-        if(movel.equalsIgnoreCase("vitoriano")){
+
+        if (movel.equalsIgnoreCase("vitoriano")) {
             factory = new Vitoriano();
-        }else{
+        } else {
             factory = new Moderno();
-        };
+        }
 
         app = new Application(factory);
-        scan.close();
+
         return app;
     }
 
