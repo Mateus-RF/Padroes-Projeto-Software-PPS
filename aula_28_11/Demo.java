ea4a46ae5d08adef54a253e1fd0c5fddb461ee0b
Mateus <mateus999391343@gmail.com>
2025-12-09 21:49:34 -0300

    Aula do Dia 28/11

---
+++ aula_28_11/Demo.java
@@ -0,0 +1,30 @@
+package aula_28_11;
+
+import java.awt.*;
+import aula_28_11.ImageEditor;
+import aula_28_11.Circle;
+import aula_28_11.Dot;
+import aula_28_11.Rectangle;
+
+public class Demo {
+    public static void main(String[] args) {
+        ImageEditor editor = new ImageEditor();
+
+        editor.loadShapes(
+                new Circle(10, 10, 10, Color.BLUE),
+
+                new CompoundShape(
+                    new Circle(110, 110, 50, Color.RED),
+                    new Dot(160, 160, Color.RED)
+                ),
+
+                new CompoundShape(
+                        new Rectangle(250, 250, 100, 100, Color.GREEN),
+                        new Dot(240, 240, Color.GREEN),
+                        new Dot(240, 360, Color.GREEN),
+                        new Dot(360, 360, Color.GREEN),
+                        new Dot(360, 240, Color.GREEN)
+                )
+        );
+    }
+}
